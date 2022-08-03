d3c6a88a69d41eb10c61da531bbc32be792cc8b0
Jordan Accinelli <jordan.accinelli@threebyone.com.au>
2022-08-03 21:56:22 +1000

    chore: shuffled everything around

---
+++ src/components/button/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./index.module.css";
+
+export default function Button() {
+  return <button className={styles.body}>Test</button>;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import Button from "./components/button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -24,11 +24,12 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <Button />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
