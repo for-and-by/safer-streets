4afc9f79a2d5186107df338f1f918778f885e60b
Jordan Accinelli <jordan.accinelli@threebyone.com.au>
2022-08-04 17:06:45 +1000

    feat: added config info

---
+++ src/config.ts
@@ -0,0 +1,27 @@
+export default {
+  seo: {
+    default: {
+      title: "Safer Streets",
+      icon: "/favicon.png",
+    },
+  },
+  views: {
+    main: "MAIN",
+    map: "MAP",
+    info: "INFO",
+    create: "CREATE",
+    search: "SEARCH",
+    filter: "FILTER",
+    hazard: "HAZARD",
+  },
+  pwa: {
+    name: "Safer Streets",
+    description: "Crowd Sourced Hazard Reporting & Information",
+    url: "https://saferstreets.info",
+    icons: {
+      sm: "/icons/icon-48x48.png",
+      md: "/icons/icon-152x152.png",
+      lg: "/icons/icon-512x512.png",
+    },
+  },
+};
