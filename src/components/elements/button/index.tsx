445ff30a81b53b6bca21023433a963a0cf4b96d0
Jordan Accinelli <jordan.accinelli@threebyone.com.au>
2022-08-04 18:59:39 +1000

    feat: cleaned up project

--- src/components/elements/button/index.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "./index.module.css";
-
-export default function Button() {
-  return <button className={styles.body}>Test</button>;
-}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import Button from "~/components/elements/button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,9 +8,6 @@ function App() {
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -24,7 +19,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <Button />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
