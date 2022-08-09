233825ab5e077962ed768794966ab23927ef2290
Jordan Accinelli <jordan.accinelli@threebyone.com.au>
2022-08-09 10:18:50 +1000

    feat: enabled toasts again

--- src/features/map/controls.tsx
+++ src/features/map/controls.tsx
@@ -1,5 +1,6 @@
 import { useTypedDispatch } from "~/features/store/hooks";
 import { zoomIn, zoomOut } from "~/features/map/store";
+import Toast from "~/features/toast/toast";
 
 // import Toast from "~/components/elements/toast";
 
@@ -16,7 +17,9 @@ export default function Controls() {
 
   return (
     <div className="flex flex-grow justify-between overflow-hidden py-4 transition-all">
-      <div className="inline-flex flex-col justify-end">{/* <Toast /> */}</div>
+      <div className="inline-flex flex-col justify-end">
+        <Toast />
+      </div>
       <div className="pointer-events-auto inline-flex flex-col space-y-2 self-end">
         <button className="btn btn-white" onClick={handleZoomIn}>
           <i className="ri-add-fill btn-icon" />
