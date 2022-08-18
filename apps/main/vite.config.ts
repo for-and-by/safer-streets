dfecf7e000b6ac1a38c319a05f5fab5731920a64
Jordan Accinelli <jordan.accinelli@threebyone.com.au>
2022-08-18 18:45:00 +1000

    fix: added icons to external rollupOptions

--- apps/main/vite.config.ts
+++ apps/main/vite.config.ts
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["icons"],
+    },
+  },
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
