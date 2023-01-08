ee20f85c984c796b7a8cd67a442c09785fd6abba
Jordan <jtaccinelli@gmail.com>
2023-01-09 01:37:24 +1000

    feat: hooked up image upload support

--- src/components/fields/image-field.tsx
+++ src/components/fields/image-field.tsx
@@ -1,16 +1,27 @@
-import Image from "~/components/inputs/image";
+import ImageInput from "~/components/inputs/image";
 import React from "react";
+import { useController, useFormContext } from "react-hook-form";
 
 interface Props {
   required?: boolean;
 }
 
 export function ImageField({ required }: Props) {
+  const { control, resetField } = useFormContext();
+  const { field } = useController({
+    name: "image",
+    control,
+    rules: { required: true },
+  });
+
   return (
-    <Image
+    <ImageInput
       placeholder={
         required ? "Upload a photo (required)" : "Upload a photo (optional)"
       }
+      value={field.value}
+      onUpload={field.onChange}
+      onRemove={() => resetField("image")}
     />
   );
 }
--- src/components/inputs/image.tsx
+++ src/components/inputs/image.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-import parseFileAsBase64 from '~/lib/parse-file-as-base64';
+import parseFileAsBase64 from "~/lib/parse-file-as-base64";
 
-import useAsync from '~/hooks/use-async';
+import useAsync from "~/hooks/use-async";
 
-import Wrapper from '~/components/inputs/wrapper';
-import WarningModal from '~/components/modals/warning';
-import Toast from '~/components/regions/toast';
+import Wrapper from "~/components/inputs/wrapper";
+import WarningModal from "~/components/modals/warning";
+import Toast from "~/components/regions/toast";
 
 interface Props {
   onUpload?: (image: string) => void;
@@ -17,92 +17,96 @@ interface Props {
   placeholder?: string;
 }
 
-export default function Image({
-	onUpload = () => {},
-	onRemove = () => {},
-	value,
-	error,
-	placeholder,
+export default function ImageInput({
+  onUpload,
+  onRemove,
+  value,
+  error,
+  placeholder,
 }: Props) {
-	const [image, setImage] = React.useState<string | undefined>(value);
-	const [file, setFile] = React.useState<File | undefined>(undefined);
-	const inputRef = React.useRef<HTMLInputElement | null>(null);
+  const [image, setImage] = React.useState<string | undefined>(value);
+  const [file, setFile] = React.useState<File | undefined>(undefined);
+  const inputRef = React.useRef<HTMLInputElement | null>(null);
 
-	const { isLoading } = useAsync(
-		async () => {
-			if (!file) return;
-			const image = await parseFileAsBase64(file);
-			setImage(image);
-			onUpload(image);
-		},
-		{ deps: [file] }
-	);
+  const { isLoading, trigger } = useAsync(async () => {
+    console.log("uploading file :>>", file);
+    if (!file) return;
+    const image = await parseFileAsBase64(file);
+    console.log("image :>>", image);
+    setImage(image);
+    if (onUpload) onUpload(image);
+  });
 
-	React.useEffect(() => {
-		setImage(value);
-	}, [value]);
+  React.useEffect(() => {
+    setImage(value);
+  }, [value]);
 
-	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		if (!event?.target?.files?.[0]) return false;
-		setFile(event.target.files[0]);
-	};
+  useEffect(() => {
+    if (file) trigger();
+  }, [file]);
 
-	const handleUpload = () => {
-		if (inputRef?.current) {
-			inputRef.current.click();
-		}
-	};
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event?.target?.files?.[0]) return false;
+    console.log(event.target.files[0]);
+    setFile(event.target.files[0]);
+  };
 
-	const handleRemove = () => {
-		setImage(undefined);
-		onRemove();
-	};
+  const handleUpload = () => {
+    if (inputRef?.current) {
+      inputRef.current.click();
+    }
+  };
 
-	return (
-		<Wrapper error={error}>
-			<Toast content="Processing Image..." show={isLoading} />
-			{!image ? (
-				<div
-					className="flex h-40 flex-grow flex-col items-center justify-center space-y-4 hover:cursor-pointer"
-					onClick={handleUpload}
-				>
-					<i className="icon icon-image-location before:text-5xl before:text-gray-400" />
-					<p className="text-gray-400">{placeholder}</p>
-				</div>
-			) : (
-				<div className="relative h-40 w-full">
-					<div className="absolute -inset-3 overflow-hidden rounded-sm">
-						<img
-							className="h-full w-full object-cover"
-							alt={file?.name ?? ''}
-							src={image ?? ''}
-						/>
-					</div>
-					<div className="absolute -inset-1">
-						<div className="absolute right-0 top-0 flex space-x-2">
-							<button className="btn btn-light" onClick={handleUpload}>
-								<i className="btn-icon icon icon-upload" />
-							</button>
-							<WarningModal
-								heading="Remove Image"
-								body="Are you sure you want to remove this image?"
-								onConfirm={handleRemove}
-							>
-								<button className="btn btn-light">
-									<i className="btn-icon icon icon-remove before:text-red-600" />
-								</button>
-							</WarningModal>
-						</div>
-					</div>
-				</div>
-			)}
-			<input
-				ref={inputRef}
-				type="file"
-				accept="image/*"
-				className="hidden"
-				onChange={handleChange}
-			/>
-		</Wrapper>
-	);
+  const handleRemove = () => {
+    setImage(undefined);
+    if (onRemove) onRemove();
+  };
+
+  return (
+    <Wrapper error={error}>
+      <Toast content="Processing Image..." show={isLoading} />
+      {!image ? (
+        <div
+          className="flex h-40 flex-grow flex-col items-center justify-center space-y-4 hover:cursor-pointer"
+          onClick={handleUpload}
+        >
+          <i className="icon icon-image-location before:text-5xl before:text-gray-400" />
+          <p className="text-gray-400">{placeholder}</p>
+        </div>
+      ) : (
+        <div className="relative h-40 w-full">
+          <div className="absolute -inset-3 overflow-hidden rounded-sm">
+            <img
+              className="h-full w-full object-cover"
+              alt={file?.name ?? ""}
+              src={image ?? ""}
+            />
+          </div>
+          <div className="absolute -inset-1">
+            <div className="absolute right-0 top-0 flex space-x-2">
+              <button className="btn btn-light" onClick={handleUpload}>
+                <i className="btn-icon icon icon-upload" />
+              </button>
+              <WarningModal
+                heading="Remove Image"
+                body="Are you sure you want to remove this image?"
+                onConfirm={handleRemove}
+              >
+                <button className="btn btn-light">
+                  <i className="btn-icon icon icon-remove before:text-red-600" />
+                </button>
+              </WarningModal>
+            </div>
+          </div>
+        </div>
+      )}
+      <input
+        ref={inputRef}
+        type="file"
+        accept="image/png,image/jpeg"
+        className="hidden"
+        onChange={handleChange}
+      />
+    </Wrapper>
+  );
 }
