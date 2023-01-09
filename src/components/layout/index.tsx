032a587716acd75c6d69911eebde1fc72c3e5d17
Jordan <jtaccinelli@gmail.com>
2023-01-09 12:51:47 +1000

    feat: clean up

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,60 +1,51 @@
-import React from 'react';
+import React from "react";
 
-import MapProvider from '~/contexts/map';
-import Map from '~/components/map/map';
-import Controls from '~/components/layout/controls';
+import MapProvider from "~/contexts/map";
+import Map from "~/components/map/map";
+import Controls from "~/components/layout/controls";
 
-import Header from '~/components/regions/header';
-import Body from '~/components/regions/body';
-import Footer from '~/components/regions/footer';
-import Toast from '~/components/regions/toast';
+import Header from "~/components/regions/header";
+import Body from "~/components/regions/body";
+import Footer from "~/components/regions/footer";
+import Toast from "~/components/regions/toast";
 
-import TopBar from '~/components/layout/top-bar';
-import BottomBar from '~/components/layout/bottom-bar';
+import TopBar from "~/components/layout/top-bar";
+import BottomBar from "~/components/layout/bottom-bar";
 
-import CenterMarker from '~/components/map/markers/center';
-import Reports from '~/components/layout/reports';
+import CenterMarker from "~/components/map/markers/center";
+import Reports from "~/components/layout/reports";
 
-import Home from '~/components/views/home';
-import Search from '~/components/views/search';
-import Create from '~/components/views/create';
-import { useForm } from 'react-hook-form';
+import Home from "~/components/views/home";
+import Search from "~/components/views/search";
+import Create from "~/components/views/create";
 
-interface Props {}
-
-export default function Index({}: Props) {
-	const {
-		register,
-		handleSubmit,
-		watch,
-		formState: { errors },
-	} = useForm();
-	return (
-		<>
-			<Home />
-			<Search />
-			<Create />
-			<div className="layer z-10">
-				<MapProvider>
-					<Map className="absolute inset-0">
-						<Reports />
-						<CenterMarker />
-					</Map>
-				</MapProvider>
-			</div>
-			<div className="layer pointer-events-none z-20">
-				<div className="clamp mx-auto flex h-full flex-col drop-shadow-lg">
-					<TopBar />
-					<Header.Container />
-					<div className="flex flex-grow justify-between overflow-hidden py-4 transition-all">
-						<Toast.Container />
-						<Controls />
-					</div>
-					<Body.Container />
-					<Footer.Container />
-					<BottomBar />
-				</div>
-			</div>
-		</>
-	);
+export default function Index() {
+  return (
+    <>
+      <Home />
+      <Search />
+      <Create />
+      <div className="layer z-10">
+        <MapProvider>
+          <Map className="absolute inset-0">
+            <Reports />
+            <CenterMarker />
+          </Map>
+        </MapProvider>
+      </div>
+      <div className="layer pointer-events-none z-20">
+        <div className="clamp mx-auto flex h-full flex-col drop-shadow-lg">
+          <TopBar />
+          <Header.Container />
+          <div className="flex flex-grow justify-between overflow-hidden py-4 transition-all">
+            <Toast.Container />
+            <Controls />
+          </div>
+          <Body.Container />
+          <Footer.Container />
+          <BottomBar />
+        </div>
+      </div>
+    </>
+  );
 }
