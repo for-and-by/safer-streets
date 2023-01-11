be13382c9d521531b39eb7a9b07b9004867d1d9e
Jordan <jtaccinelli@gmail.com>
2023-01-11 16:20:08 +1000

    feat: reverted typescript

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,15 @@ module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
-			colors: require('~/lib/colors'),
+			colors: {
+				action: require('tailwindcss/colors').blue,
+				warning: require('tailwindcss/colors').amber,
+				danger: require('tailwindcss/colors').red,
+				error: require('tailwindcss/colors').red,
+				gray: require('tailwindcss/colors').stone,
+				base: require('tailwindcss/colors').stone,
+				brand: require('tailwindcss/colors').emerald,
+			},
 			fontFamily: {
 				sans: ['Inter', 'Helvetica', 'system-elements'],
 			},
