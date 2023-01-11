df1ccce7454b820e050c43eee1171778acfaeb10
Jordan <jtaccinelli@gmail.com>
2023-01-11 13:50:08 +1000

    feat: updated to use native layers to render items

--- src/components/layout/reports/index.tsx
+++ src/components/layout/reports/index.tsx
@@ -3,18 +3,41 @@ import useReports from "~/hooks/reports/use-reports";
 import useReportSync from "~/hooks/reports/use-report-sync";
 
 import Toast from "~/components/regions/toast";
-import MarkerFactory from "~/components/map/markers/factory";
-import ReportMarker from "~/components/map/markers/report";
-import { Report } from "~/types/db";
-import ClusterMarker from "~/components/map/markers/cluster";
-import parseLngLat from "~/lib/parse-lng-lat";
 import useMapSource from "~/hooks/map/use-map-source";
 import useMapLayer from "~/hooks/map/use-map-layer";
 import parseReportsAsGeoJSON from "~/lib/parse-reports-as-geojson";
+import useMapImages from "~/hooks/map/use-map-images";
 
 export default function Reports() {
   const reports = useReports();
   const { syncReports, isSyncing } = useReportSync();
+
+  useMapImages([
+    {
+      id: "cyclist",
+      url: "/icons/cyclist.png",
+    },
+    {
+      id: "fire",
+      url: "/icons/fire.png",
+    },
+    {
+      id: "flood",
+      url: "/icons/flood.png",
+    },
+    {
+      id: "motorist",
+      url: "/icons/motorist.png",
+    },
+    {
+      id: "pedestrian",
+      url: "/icons/pedestrian.png",
+    },
+    {
+      id: "wildlife",
+      url: "/icons/wildlife.png",
+    },
+  ]);
 
   useMapSource({
     id: "reports",
@@ -24,12 +47,47 @@ export default function Reports() {
   });
 
   useMapLayer({
-    id: "reports",
+    id: "clusters-bg",
     type: "circle",
     source: "reports",
+    filter: ["has", "point_count"],
     paint: {
-      "circle-radius": 0,
-      "circle-color": "#FFF",
+      "circle-color": "red",
+      "circle-radius": 20,
+    },
+  });
+
+  useMapLayer({
+    id: "clusters-icon",
+    type: "symbol",
+    source: "reports",
+    filter: ["has", "point_count"],
+    layout: {
+      "text-field": "{point_count_abbreviated}",
+      "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+      "text-size": 12,
+    },
+  });
+
+  useMapLayer({
+    id: "reports-bg",
+    type: "circle",
+    source: "reports",
+    filter: ["!", ["has", "point_count"]],
+    paint: {
+      "circle-color": "black",
+      "circle-radius": 20,
+    },
+  });
+
+  useMapLayer({
+    id: "reports-icon",
+    type: "symbol",
+    source: "reports",
+    filter: ["!", ["has", "point_count"]],
+    layout: {
+      "icon-image": "{type_handle}",
+      "icon-size": 0.2,
     },
   });
 
@@ -40,30 +98,30 @@ export default function Reports() {
   return (
     <>
       <Toast content={"Syncing Reports..."} show={isSyncing} />
-      <MarkerFactory
-        source="reports"
-        filter={(marker) => !marker.properties.cluster}
-        render={({ geometry, properties }) =>
-          geometry.type === "Point" ? (
-            <ReportMarker
-              coordinates={parseLngLat(geometry.coordinates)}
-              report={properties as Report}
-            />
-          ) : null
-        }
-      />
-      <MarkerFactory
-        source="reports"
-        filter={(marker) => marker.properties.cluster}
-        render={({ geometry, properties }) =>
-          geometry.type === "Point" && (
-            <ClusterMarker
-              coordinates={parseLngLat(geometry.coordinates)}
-              count={properties.point_count}
-            />
-          )
-        }
-      />
+      {/*<MarkerFactory*/}
+      {/*  source="reports"*/}
+      {/*  filter={(marker) => !marker.properties.cluster}*/}
+      {/*  render={({ geometry, properties }) =>*/}
+      {/*    geometry.type === "Point" ? (*/}
+      {/*      <ReportMarker*/}
+      {/*        coordinates={parseLngLat(geometry.coordinates)}*/}
+      {/*        report={properties as Report}*/}
+      {/*      />*/}
+      {/*    ) : null*/}
+      {/*  }*/}
+      {/*/>*/}
+      {/*<MarkerFactory*/}
+      {/*  source="reports"*/}
+      {/*  filter={(marker) => marker.properties.cluster}*/}
+      {/*  render={({ geometry, properties }) =>*/}
+      {/*    geometry.type === "Point" && (*/}
+      {/*      <ClusterMarker*/}
+      {/*        coordinates={parseLngLat(geometry.coordinates)}*/}
+      {/*        count={properties.point_count}*/}
+      {/*      />*/}
+      {/*    )*/}
+      {/*  }*/}
+      {/*/>*/}
     </>
   );
 }
