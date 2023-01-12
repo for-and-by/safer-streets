2cad87990034efc663fe1c81136a342c014ccfa4
Jordan <jtaccinelli@gmail.com>
2023-01-13 01:24:18 +1000

    feat: added a force sync button

--- src/components/layout/controls.tsx
+++ src/components/layout/controls.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import useMapZoom from '~/hooks/map/use-map-zoom';
+import React from "react";
+import useMapZoom from "~/hooks/map/use-map-zoom";
+import useReportSync from "~/hooks/reports/use-report-sync";
 
 export default function Controls() {
-	const [, { zoomIn, zoomOut }] = useMapZoom();
+  const [, { zoomIn, zoomOut }] = useMapZoom();
+  const { syncReports } = useReportSync();
 
-	return (
-		<div className="pointer-events-auto inline-flex flex-col space-y-2 self-end">
-			<button className="btn btn-white" onClick={zoomIn}>
-				<i className="icon icon-plus btn-icon" />
-			</button>
-			<button className="btn btn-white" onClick={zoomOut}>
-				<i className="icon icon-minus btn-icon" />
-			</button>
-		</div>
-	);
+  return (
+    <div className="pointer-events-auto inline-flex flex-col space-y-2 self-end">
+      <button className="btn btn-white" onClick={() => syncReports().finally()}>
+        <i className="icon icon-map-target btn-icon" />
+      </button>
+      <button className="btn btn-white" onClick={zoomIn}>
+        <i className="icon icon-plus btn-icon" />
+      </button>
+      <button className="btn btn-white" onClick={zoomOut}>
+        <i className="icon icon-minus btn-icon" />
+      </button>
+    </div>
+  );
 }
