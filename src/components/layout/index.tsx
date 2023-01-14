2b12f02247b32249eccf44e8e47a2e4446d6b127
Jordan <jtaccinelli@gmail.com>
2023-01-14 22:30:34 +1000

    fix: moved map provider up and out to wrap the entire application

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -8,17 +8,20 @@ import Toast from "~/components/regions/toast";
 import TopBar from "~/components/layout/top-bar";
 import BottomBar from "~/components/layout/bottom-bar";
 import Controls from "~/components/layout/controls";
-import MapLayer from "~/components/layout/map/layer";
 
 import Home from "~/components/views/home";
 import Search from "~/components/views/search";
 import Create from "~/components/views/create";
 import CreateProvider from "~/components/views/create/context";
 import Report from "~/components/views/report";
+import MapProvider from "~/components/layout/map/context";
+import Reports from "~/components/layout/reports";
+import CenterMarker from "~/components/layout/map/markers/center";
+import Map from "~/components/layout/map";
 
 export default function Index() {
   return (
-    <>
+    <MapProvider>
       <Home />
       <Search />
       <Report />
@@ -26,7 +29,10 @@ export default function Index() {
         <Create />
       </CreateProvider>
       <div className="layer z-10">
-        <MapLayer />
+        <Map className="absolute inset-0">
+          <Reports />
+          <CenterMarker />
+        </Map>
       </div>
       <div className="layer pointer-events-none z-20">
         <div className="clamp mx-auto flex h-full flex-col drop-shadow-lg">
@@ -41,6 +47,6 @@ export default function Index() {
           <BottomBar />
         </div>
       </div>
-    </>
+    </MapProvider>
   );
 }
---
+++ src/components/layout/map/markers/report.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import BaseMarker from "~/components/layout/map/markers/base";
+import useViewIsActive from "~/hooks/view/use-view-is-active";
+import { VIEWS } from "~/hooks/view/use-view-store";
+import { LngLatLike } from "maplibre-gl";
+
+interface Props {
+  coordinates: LngLatLike;
+}
+
+export default function ReportMarker({ coordinates }: Props) {
+  const isReportActive = useViewIsActive(VIEWS.REPORT);
+
+  if (!coordinates || !isReportActive) return null;
+
+  return (
+    <BaseMarker coordinates={coordinates} anchor="bottom-right">
+      <div className="relative flex h-8 w-8 origin-bottom-right rotate-45 items-center justify-center rounded-full rounded-br-none bg-emerald-600 text-white transition-all">
+        <i className="icon icon-sm icon-pin-fill -rotate-45" />
+      </div>
+    </BaseMarker>
+  );
+}
