4c9df4d033a34367ac51314aa24c4f1df2969db6
Jordan <jtaccinelli@gmail.com>
2023-02-13 12:32:26 +1000

    feat: updated search to use remix native functions

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -1,118 +1,141 @@
 import React, { useEffect, useState } from "react";
 import type { ActionFunction } from "@remix-run/router";
-import { useNavigate, useTransition } from "@remix-run/react";
-import useMapCenter from "~/hooks/map/use-map-center";
-import useGeocoderQuery from "~/hooks/geocoder/use-geocoder-query";
-import useGeocoderResults from "~/hooks/geocoder/use-geocoder-results";
-import useGeocoderReset from "~/hooks/geocoder/use-geocoder-reset";
-import useDebounce from "~/hooks/use-debounce";
+import { nanoid } from "nanoid";
+
+import {
+  Link,
+  useActionData,
+  useNavigate,
+  useSubmit,
+  useTransition,
+} from "@remix-run/react";
+
 import type { SearchFeature } from "~/types/search";
+import geocode from "~/lib/geocode";
+
+import useMapCenter from "~/hooks/map/use-map-center";
+
 import Toast from "~/components/regions/toast";
 import Header from "~/components/regions/header";
 import Body from "~/components/regions/body";
-import Bumper from "~/components/elements/bumper";
-import { nanoid } from "nanoid";
 import Footer from "~/components/regions/footer";
+
 import Text from "~/components/inputs/text";
+
 import FindSelfButton from "~/components/elements/find-self-button";
+import Bumper from "~/components/elements/bumper";
+import Show from "~/components/elements/show";
+
+interface SearchResponse {
+  results: SearchFeature[];
+  resultsCount: number;
+  isEmpty: boolean;
+}
+
+function generateSearchResponse(features: SearchFeature[]): SearchResponse {
+  return {
+    results: features,
+    resultsCount: features.length,
+    isEmpty: features.length === 0,
+  };
+}
 
 export const action: ActionFunction = async ({ request }) => {
   const data = await request.formData();
-  return { search: data };
+  const query = data.get("query");
+
+  if (!query || typeof query !== "string") {
+    return generateSearchResponse([]);
+  }
+
+  const features = await geocode(query);
+  return generateSearchResponse(features);
 };
 
 export default function Search() {
+  const { state, type } = useTransition();
+  const search = useActionData<SearchResponse>();
+
   const navigate = useNavigate();
-  const { state } = useTransition();
+  const submit = useSubmit();
+
   const [, setCenter] = useMapCenter();
-
-  const [query, setQuery] = useGeocoderQuery();
-  const [{ results, isEmpty, resultsCount, isLoading }, { fetchResults }] =
-    useGeocoderResults();
-  const resetGeocoder = useGeocoderReset();
-
   const [value, setValue] = useState<string>("");
-  const debouncedValue = useDebounce(value, 500);
-
-  const [content, setContent] = useState({
-    heading: "",
-    subheading: "",
-  });
 
   useEffect(() => {
-    fetchResults().finally();
-  }, [query]);
+    const timeout = setTimeout(() => {
+      const data = new FormData();
+      data.append("query", value);
+      submit(data, { method: "post", action: "/search" });
+    }, 400);
 
-  useEffect(() => {
-    setQuery(debouncedValue);
-  }, [debouncedValue]);
+    return () => {
+      clearTimeout(timeout);
+    };
 
-  useEffect(() => {
-    setContent({
-      heading: `${isEmpty ? "No" : resultsCount} results found`,
-      subheading: isEmpty
-        ? "Select a location to jump to."
-        : "Start by typing in an address.",
-    });
-  }, [results]);
-
-  const handleExitSearch = () => {
-    resetGeocoder();
-    navigate("/");
-  };
+    //  eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value]);
 
   const handleClickResult = (feature: SearchFeature) => {
     if (feature.center) setCenter(feature.center);
-    handleExitSearch();
+    navigate("/");
   };
 
   return (
     <>
-      <Toast content="Finding results..." show={isLoading} />
+      <Toast content="Finding results..." show={state === "submitting"} />
       <Header
-        show={state === "idle"}
+        show={type !== "normalRedirect"}
         className="flex flex-row items-center bg-white p-2"
       >
-        <button className="btn btn-light" onClick={handleExitSearch}>
+        <Link to="/" className="btn btn-light">
           <i className="btn-icon icon icon-left" />
-        </button>
+        </Link>
         <div className="flex flex-col px-3">
-          <h3 className="text-base text-base-700">{content.heading}</h3>
-          <p className="text-sm text-base-400">{content.subheading}</p>
+          <h3 className="text-base text-base-700">
+            {!search || search.isEmpty ? "No" : search.resultsCount} results
+            found
+          </h3>
+          <p className="text-sm text-base-400">
+            <Show on={!search || search.isEmpty}>
+              Start by typing in an address.
+            </Show>
+            <Show on={!!search && !search.isEmpty}>
+              Select a location to jump to.
+            </Show>
+          </p>
         </div>
       </Header>
       <Body>
         <Bumper
-          show={results.length > 0}
+          show={!search?.isEmpty}
           className="flex max-h-48 flex-col items-center divide-y divide-base-100 overflow-y-scroll bg-white"
         >
-          {results?.map((feature) =>
+          {search?.results.map((feature) =>
             feature.center ? (
               <button
                 key={nanoid()}
                 onClick={() => handleClickResult(feature)}
                 className="flex w-full flex-col bg-white p-3 transition-all hover:cursor-pointer hover:bg-gray-100"
               >
-                <p className="text-base text-base-700">{feature?.heading}</p>
-                <p className="text-sm text-base-400">{feature?.subheading}</p>
+                <p className="text-base text-base-700">{feature.heading}</p>
+                <p className="text-sm text-base-400">{feature.subheading}</p>
               </button>
             ) : null
           )}
         </Bumper>
       </Body>
       <Footer
-        show={state === "idle"}
+        show={type !== "normalRedirect"}
         className="flex flex-row items-center space-x-2 bg-white p-2"
       >
-        <form className="flex-grow">
-          <Text
-            value={value}
-            onChange={(event) => setValue(event.target.value)}
-            icon="icon-search"
-            placeholder="Search for an address..."
-          />
-        </form>
-        <FindSelfButton onFound={handleExitSearch} />
+        <Text
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+          icon="icon-search"
+          placeholder="Search for an address..."
+        />
+        <FindSelfButton onFound={() => navigate("/")} />
       </Footer>
     </>
   );
