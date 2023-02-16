55c3bf30c6f4134ad7a78533b7b94983592112c6
Jordan <jtaccinelli@gmail.com>
2023-02-16 12:44:01 +1000

    fix: removed error messaging

--- app/errors.ts
+++ app/errors.ts
@@ -1,16 +1,6 @@
 export default {
-	map: {
-		CANT_ZOOM_IN: 'You\'ve zoomed in to the max. Try zooming out instead',
-		CANT_ZOOM_OUT: 'You\'ve zoomed out to the max. Try zooming out instead',
-	},
-	view: {
-		ALREADY_EXISTS:
-      'A view with this key already exists. Please check that you are using view with a unique key.',
-		DOESNT_EXIST:
-      'A view with this key could not be removed as it has not been registered. Please check the spelling and case of your key.',
-		ALREADY_EMPTY:
-      'The view array could not be cleared, as no views were registered.',
-		CANNOT_FOCUS:
-      'A view with this key could not be focused, as it does not exist.',
-	},
+  map: {
+    CANT_ZOOM_IN: "You've zoomed in to the max. Try zooming out instead",
+    CANT_ZOOM_OUT: "You've zoomed out to the max. Try zooming out instead",
+  },
 };
