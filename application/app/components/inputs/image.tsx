d17e8075f0cc5a05b24058b489a8d6a36ae6baeb
Jordan <jtaccinelli@gmail.com>
2023-02-23 16:30:38 +1000

    feat: fixed icons not loading errors

--- application/app/components/inputs/image.tsx
+++ application/app/components/inputs/image.tsx
@@ -1,112 +1,112 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from 'react';
 
-import useAsync from "~/hooks/use-async";
+import useAsync from '~/hooks/use-async';
 
-import Wrapper from "~/components/inputs/wrapper";
+import Wrapper from '~/components/inputs/wrapper';
 
-import { Warning } from "~/components/composites/warning";
-import Toast from "~/components/regions/toast";
-import type { FieldError } from "react-hook-form";
-import { parseFileAsBase64 } from "~/lib/image";
+import {Warning} from '~/components/composites/warning';
+import Toast from '~/components/regions/toast';
+import type {FieldError} from 'react-hook-form';
+import {parseFileAsBase64} from '~/lib/image';
 
 interface Props {
-  onUpload?: (image: string, file: File) => void;
-  onRemove?: () => void;
+	onUpload?: (image: string, file: File) => void;
+	onRemove?: () => void;
 
-  error?: FieldError;
-  value?: string;
-  placeholder?: string;
+	error?: FieldError;
+	value?: string;
+	placeholder?: string;
 }
 
 export default function ImageInput({
-  onUpload,
-  onRemove,
-  value,
-  error,
-  placeholder,
+	onUpload,
+	onRemove,
+	value,
+	error,
+	placeholder,
 }: Props) {
-  const [image, setImage] = React.useState<string | undefined>(value);
-  const [file, setFile] = React.useState<File | undefined>(undefined);
-  const inputRef = React.useRef<HTMLInputElement | null>(null);
+	const [image, setImage] = React.useState<string | undefined>(value);
+	const [file, setFile] = React.useState<File | undefined>(undefined);
+	const inputRef = React.useRef<HTMLInputElement | null>(null);
 
-  const { isLoading, trigger } = useAsync(async () => {
-    if (!file) return;
-    const image = await parseFileAsBase64(file);
-    setImage(image);
-    if (onUpload) onUpload(image, file);
-  });
+	const {isLoading, trigger} = useAsync(async () => {
+		if (!file) return;
+		const image = await parseFileAsBase64(file);
+		setImage(image);
+		if (onUpload) onUpload(image, file);
+	});
 
-  React.useEffect(() => {
-    setImage(value);
-  }, [value]);
+	React.useEffect(() => {
+		setImage(value);
+	}, [value]);
 
-  useEffect(() => {
-    if (file) trigger();
-    //   eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [file]);
+	useEffect(() => {
+		if (file) trigger();
+		//   eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [file]);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (!event?.target?.files?.[0]) return false;
-    setFile(event.target.files[0]);
-  };
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		if (!event?.target?.files?.[0]) return false;
+		setFile(event.target.files[0]);
+	};
 
-  const handleUpload = () => {
-    if (inputRef?.current) {
-      inputRef.current.click();
-    }
-  };
+	const handleUpload = () => {
+		if (inputRef?.current) {
+			inputRef.current.click();
+		}
+	};
 
-  const handleRemove = () => {
-    setImage(undefined);
-    if (onRemove) onRemove();
-  };
+	const handleRemove = () => {
+		setImage(undefined);
+		if (onRemove) onRemove();
+	};
 
-  return (
-    <Warning>
-      <Wrapper error={error}>
-        <Toast content="Processing Image..." show={isLoading} />
-        {!image ? (
-          <div
-            className="flex h-40 flex-grow flex-col items-center justify-center space-y-4 hover:cursor-pointer"
-            onClick={handleUpload}
-          >
-            <i className="icon icon-image-location before:text-5xl before:text-gray-400" />
-            <p className="text-gray-400">{placeholder}</p>
-          </div>
-        ) : (
-          <div className="relative h-40 w-full">
-            <div className="absolute -inset-3 overflow-hidden rounded-sm">
-              <img
-                className="h-full w-full object-cover"
-                alt={file?.name ?? ""}
-                src={image ?? ""}
-              />
-            </div>
-            <div className="absolute -inset-1">
-              <div className="absolute right-0 top-0 flex space-x-2">
-                <button className="btn btn-light" onClick={handleUpload}>
-                  <i className="btn-icon icon icon-upload" />
-                </button>
-                <Warning.Trigger className="btn btn-light">
-                  <i className="btn-icon icon icon-remove before:text-red-600" />
-                </Warning.Trigger>
-              </div>
-            </div>
-          </div>
-        )}
-        <input
-          ref={inputRef}
-          type="file"
-          accept="image/png,image/jpeg"
-          className="hidden"
-          onChange={handleChange}
-        />
-      </Wrapper>
-      <Warning.Panel
-        heading="Remove Image"
-        body="Are you sure you want to remove this image?"
-        onConfirm={handleRemove}
-      />
-    </Warning>
-  );
+	return (
+		<Warning>
+			<Wrapper error={error}>
+				<Toast content="Processing Image..." show={isLoading}/>
+				{!image ? (
+					<div
+						className="flex h-40 flex-grow flex-col items-center justify-center space-y-4 hover:cursor-pointer"
+						onClick={handleUpload}
+					>
+						<i className="icon icon-image before:text-5xl before:text-gray-400"/>
+						<p className="text-gray-400">{placeholder}</p>
+					</div>
+				) : (
+					<div className="relative h-40 w-full">
+						<div className="absolute -inset-3 overflow-hidden rounded-sm">
+							<img
+								className="h-full w-full object-cover"
+								alt={file?.name ?? ''}
+								src={image ?? ''}
+							/>
+						</div>
+						<div className="absolute -inset-1">
+							<div className="absolute right-0 top-0 flex space-x-2">
+								<button className="btn btn-white" onClick={handleUpload}>
+									<i className="btn-icon icon icon-save-over"/>
+								</button>
+								<Warning.Trigger className="btn btn-white">
+									<i className="btn-icon icon icon-trash before:text-red-600"/>
+								</Warning.Trigger>
+							</div>
+						</div>
+					</div>
+				)}
+				<input
+					ref={inputRef}
+					type="file"
+					accept="image/png,image/jpeg"
+					className="hidden"
+					onChange={handleChange}
+				/>
+			</Wrapper>
+			<Warning.Panel
+				heading="Remove Image"
+				body="Are you sure you want to remove this image?"
+				onConfirm={handleRemove}
+			/>
+		</Warning>
+	);
 }
--- application/app/components/inputs/select.tsx
+++ application/app/components/inputs/select.tsx
@@ -1,57 +1,57 @@
-import type { ComponentProps } from "react";
-import React, { forwardRef } from "react";
+import type {ComponentProps} from 'react';
+import React, {forwardRef} from 'react';
 
-import Wrapper from "~/components/inputs/wrapper";
-import type { FieldError } from "react-hook-form";
+import Wrapper from '~/components/inputs/wrapper';
+import type {FieldError} from 'react-hook-form';
 
-interface Props extends ComponentProps<"select"> {
-  label: string;
-  options?: {
-    value: string;
-    label: string;
-  }[];
-  loading?: boolean;
-  placeholder?: string;
-  error?: FieldError;
+interface Props extends ComponentProps<'select'> {
+	label: string;
+	options?: {
+		value: string;
+		label: string;
+	}[];
+	loading?: boolean;
+	placeholder?: string;
+	error?: FieldError;
 }
 
 const Select = forwardRef<HTMLSelectElement, Props>(
-  ({ label, options, name, loading, placeholder, error, ...props }, ref) => {
-    return (
-      <Wrapper label={label} name={name} error={error}>
-        <select
-          className="flex-grow appearance-none bg-transparent focus:outline-none"
-          name={name}
-          {...props}
-          ref={ref}
-        >
-          {loading ? (
-            <option disabled hidden value="">
-              {placeholder}
-            </option>
-          ) : (
-            <option disabled hidden value="">
-              Please select...
-            </option>
-          )}
-          {options?.map((option) => {
-            return (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            );
-          })}
-        </select>
-        {loading ? (
-          <i className="icon icon-is-spinning icon-circle-anim z-20 text-gray-700" />
-        ) : (
-          <i className="icon icon-down icon-sm absolute right-2 top-1/2 -translate-y-1/2 text-gray-700" />
-        )}
-      </Wrapper>
-    );
-  }
+	({label, options, name, loading, placeholder, error, ...props}, ref) => {
+		return (
+			<Wrapper label={label} name={name} error={error}>
+				<select
+					className="flex-grow appearance-none bg-transparent focus:outline-none"
+					name={name}
+					{...props}
+					ref={ref}
+				>
+					{loading ? (
+						<option disabled hidden value="">
+							{placeholder}
+						</option>
+					) : (
+						<option disabled hidden value="">
+							Please select...
+						</option>
+					)}
+					{options?.map((option) => {
+						return (
+							<option key={option.value} value={option.value}>
+								{option.label}
+							</option>
+						);
+					})}
+				</select>
+				{loading ? (
+					<i className="icon icon-is-spinning icon-circle-anim z-20 text-gray-700"/>
+				) : (
+					<i className="icon icon-arrow-down icon-sm absolute right-4 top-1/2 -translate-y-1/2 text-gray-700"/>
+				)}
+			</Wrapper>
+		);
+	}
 );
 
-Select.displayName = "Select";
+Select.displayName = 'Select';
 
 export default Select;
