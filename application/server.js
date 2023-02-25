a9c8257cbf9b25c99efb3e5a23e282cc6d6dc13a
Jordan <jtaccinelli@gmail.com>
2023-02-25 16:44:31 +1000

    feat: dropped session data

--- application/server.js
+++ application/server.js
@@ -1,29 +1,14 @@
-import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
-import * as build from "@remix-run/dev/server-build";
-import {
-  createCookie,
-  createWorkersKVSessionStorage,
-} from "@remix-run/cloudflare";
-
-const sessionCookie = createCookie("__session", {
-  sameSite: "strict",
-  secure: true,
-  maxAge: 604_800,
-});
+import {createPagesFunctionHandler} from '@remix-run/cloudflare-pages';
+import * as build from '@remix-run/dev/server-build';
 
 const handleRequest = createPagesFunctionHandler({
-  build,
-  mode: process.env.NODE_ENV,
-  getLoadContext: (context) => {
-    const sessionStorage = createWorkersKVSessionStorage({
-      cookie: sessionCookie,
-      kv: context.env.SESSIONS,
-    });
-
-    return { env: context.env, sessionStorage };
-  },
+	build,
+	mode: process.env.NODE_ENV,
+	getLoadContext: (context) => {
+		return {env: context.env};
+	},
 });
 
 export function onRequest(context) {
-  return handleRequest(context);
+	return handleRequest(context);
 }
