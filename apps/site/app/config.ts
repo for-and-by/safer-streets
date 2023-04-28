4021fbcd6cbf3ec50e993f5777ecb3fc69ec3c38
Jordan <jtaccinelli@gmail.com>
2023-04-29 01:03:34 +1000

    feat: brought all stuff required across to new project

---
+++ apps/site/app/config.ts
@@ -0,0 +1,18 @@
+export const config = {
+  seo: {
+    default: {
+      title: "Safer Streets",
+      description:
+        "Safer Streets is a community managed hazard reporting platform that helps individuals to share safety issues with their local area.",
+    },
+  },
+  css: {
+    fonts:
+      "https://fonts.googleapis.com/css2?family=Inter:wght@300..800&display=swap",
+  },
+  pwa: {
+    name: "Safer Streets",
+    description: "Crowd Sourced Hazard Reporting & Information",
+    url: "https://saferstreets.info",
+  },
+};
---
+++ apps/site/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+
+const baseConfig = require("@safer-streets/tailwind/config");
+
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  ...baseConfig,
+};
