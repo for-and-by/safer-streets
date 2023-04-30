03b83df8625edbbe9989534204f68eb5d4fe63a9
Jordan <jtaccinelli@gmail.com>
2023-04-30 13:15:15 +1000

    feat: added beige to tailwind

--- packages/tailwind/src/tailwind.config.cjs
+++ packages/tailwind/src/tailwind.config.cjs
@@ -1,28 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        action: require("tailwindcss/colors").blue,
-        warning: require("tailwindcss/colors").amber,
-        danger: require("tailwindcss/colors").red,
-        error: require("tailwindcss/colors").red,
-        gray: require("tailwindcss/colors").stone,
-        base: require("tailwindcss/colors").stone,
-        brand: require("tailwindcss/colors").emerald,
-      },
-      fontFamily: {
-        sans: ["Inter", "Helvetica", "system-elements"],
-      },
-    },
-  },
-  plugins: [
-    // Add custom variant classes
-    require("@tailwindcss/typography"),
-    require("tailwindcss/plugin")(({ addVariant }) => {
-      addVariant("child", "& > *");
-      addVariant("d-active", "&[data-active]");
-    }),
-  ],
+	theme: {
+		extend: {
+			colors: {
+				action: require('tailwindcss/colors').blue,
+				warning: require('tailwindcss/colors').amber,
+				danger: require('tailwindcss/colors').red,
+				error: require('tailwindcss/colors').red,
+				gray: require('tailwindcss/colors').stone,
+				base: require('tailwindcss/colors').stone,
+				brand: require('tailwindcss/colors').emerald,
+				beige: {
+					DEFAULT: '#F5F2E6'
+				}
+			},
+			fontFamily: {
+				sans: ['Inter', 'Helvetica', 'system-elements'],
+			},
+		},
+	},
+	plugins: [
+		// Add custom variant classes
+		require('@tailwindcss/typography'),
+		require('tailwindcss/plugin')(({addVariant}) => {
+			addVariant('child', '& > *');
+			addVariant('d-active', '&[data-active]');
+		}),
+	],
 };
