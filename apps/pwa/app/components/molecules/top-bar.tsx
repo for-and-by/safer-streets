5ac3a388212b46092e09c4ff680ddd7dba7a8cea
Jordan <jtaccinelli@gmail.com>
2023-04-30 23:03:02 +1000

    feat: dropped content form main app

--- apps/pwa/app/components/molecules/top-bar.tsx
+++ apps/pwa/app/components/molecules/top-bar.tsx
@@ -5,11 +5,17 @@ export default function TopBar() {
   return (
     <div className="pointer-events-auto flex w-full flex-row bg-base-100">
       <div className="flex flex-grow flex-row space-x-4 py-1 px-2">
-        <p className="text-sm text-base-900">
+        <Link
+          to="https://saferstreets.info/content/disclaimer"
+          className="text-sm text-base-900"
+        >
           Endorsed By Jonathan Sriranganathan
-        </p>
+        </Link>
       </div>
-      <Link to="/about" className="flex h-full items-center px-2">
+      <Link
+        to="https://saferstreets.info/content/about"
+        className="flex h-full items-center px-2"
+      >
         <i className="icon icon-info icon-sm before:text-brand-700" />
       </Link>
     </div>
--- apps/pwa/app/routes/about/contact.md
+++
@@ -1,4 +0,0 @@
-Having trouble, or want to make a recommendation or request?
-
-You can get send all contact requests through to
-[info@jtaccinelli.com.au](mailto://info@jtaccinelli.com.au)
