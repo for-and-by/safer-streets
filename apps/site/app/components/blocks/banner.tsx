528a48dc797e8e1141188c792f11f2e0749e897b
Jordan <jtaccinelli@gmail.com>
2023-05-01 00:23:40 +1000

    feat: fixed mobile layouts

--- apps/site/app/components/blocks/banner.tsx
+++ apps/site/app/components/blocks/banner.tsx
@@ -15,8 +15,14 @@ export function BannerBlock({ data }: Props) {
   return (
     <div className="flex flex-row items-center gap-4 rounded-md bg-gray-900 p-4 text-white">
       <i className={clsx("icon before:text-brand-600", data?.icon)} />
-      <p className="flex-grow">{data.text}</p>
-      <Link to={data.cta.url}>{data.cta.text}</Link>
+      <p className="flex-grow truncate">{data.text}</p>
+      <Link
+        className="flex items-center gap-2 whitespace-nowrap"
+        to={data.cta.url}
+      >
+        <span className="hidden md:block">{data.cta.text}</span>
+        <i className="icon icon-arrow-right before:text-xs" />
+      </Link>
     </div>
   );
 }
