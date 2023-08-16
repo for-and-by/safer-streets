6880515fabd24d63ac8426283291c2aa5f8d6ead
Jordan <jtaccinelli@gmail.com>
2023-08-16 16:58:22 +1000

    fix: error exports

--- apps/pwa/app/errors.ts
+++ apps/pwa/app/errors.ts
@@ -1,6 +1,7 @@
-export default {
-  map: {
-    CANT_ZOOM_IN: "You've zoomed in to the max. Try zooming out instead",
-    CANT_ZOOM_OUT: "You've zoomed out to the max. Try zooming out instead",
-  },
-};
+export const CANT_ZOOM_IN =
+  "You've zoomed in to the max. Try zooming out instead";
+
+export const CANT_ZOOM_OUT =
+  "You've zoomed out to the max. Try zooming out instead";
+
+export const CANT_GEOLOCATE = "Geolocation not available on this browser.";
